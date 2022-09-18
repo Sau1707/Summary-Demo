37cff1be76e2787fc1433d7c1312934b0dc01a09
Sau1707 <Sau1707@users.noreply.github.com>
2022-09-18 22:24:59 +0000

    deploy: 86a25f46f2a4864edf68265e029ed30a751a7bfc

--- _next/static/FIfB271AgVxWWzheCcnEb/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c0f88e45e19f2380.css","static/chunks/pages/index-8bbc2c0f16c74c7a.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
---
+++ _next/static/aXoNPOYgodgBJsmc1cQYr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/198-3a64bc4446ca841f.js","static/css/5f7ba07d26537a32.css","static/chunks/pages/index-06b1f70aaf77c89d.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
